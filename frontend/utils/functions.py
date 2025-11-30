caeb68b924f15412dc1d4753c29bd76635702e96
Tom Oguntuga <adetomiwa.oguntuga@gmail.com>
2025-11-30 10:46:22 -0600

    Added docker compose file

--- frontend/utils/functions.py
+++ frontend/utils/functions.py
@@ -2,7 +2,7 @@ import requests
 from utils.resources import local_base_url, docker_base_url
 
 def send_user_query(user_query):
-    kalex_query_endpoint = f"{local_base_url}/kalex/query_kalex"
+    kalex_query_endpoint = f"{docker_base_url}/kalex/query_kalex"
     response = requests.get(kalex_query_endpoint, params={"user_prompt":user_query})
     
     if response.status_code == 200:
